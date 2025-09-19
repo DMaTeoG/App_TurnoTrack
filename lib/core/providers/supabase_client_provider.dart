9b35d9abaae6f7152adf97d57da49b48423f09a7
DMaTeoG <david.guerreroi@campusucc.edu.co>
2025-09-19 09:05:00 -0500

    feat: exponer providers de supabase y conectividad

---
+++ lib/core/providers/supabase_client_provider.dart
@@ -0,0 +1,17 @@
+﻿import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
+
+final supabaseClientProvider = Provider<SupabaseClient>((ref) {
+  return Supabase.instance.client;
+});
+
+final sessionStreamProvider = StreamProvider<Session?>((ref) {
+  final client = ref.watch(supabaseClientProvider);
+  return client.auth.onAuthStateChange.map((data) => data.session);
+});
+
+final currentSessionProvider = Provider<Session?>((ref) {
+  final asyncSession = ref.watch(sessionStreamProvider);
+  return asyncSession.value ?? Supabase.instance.client.auth.currentSession;
+});
+
