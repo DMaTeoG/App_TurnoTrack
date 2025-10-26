f90fd9233bead7ba9a9276d235b208217fca15fb
DMaTeoG <david.guerreroi@campusucc.edu.co>
2025-10-26 10:32:14 -0500

    credenciales

--- lib/core/config/constants.dart
+++ lib/core/config/constants.dart
@@ -1,8 +1,8 @@
 ﻿class AppConstants {
   static const supabaseUrl =
-      String.fromEnvironment('SUPABASE_URL', defaultValue: '');
+      String.fromEnvironment('https://ixfdivaywaixpqcpednr.supabase.co', defaultValue: '');
   static const supabaseAnonKey =
-      String.fromEnvironment('SUPABASE_ANON_KEY', defaultValue: '');
+      String.fromEnvironment('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Iml4ZmRpdmF5d2FpeHBxY3BlZG5yIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjE0NDQ4NjcsImV4cCI6MjA3NzAyMDg2N30.c_CKH5aCNjz38_eKNGf4zIzdlVALeXg7MBXMWHlj2HE', defaultValue: '');
 
   static const registrosBucket = 'registros';
   static const exportsBucket = 'exports';
